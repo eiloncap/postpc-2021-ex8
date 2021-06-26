a93a26e4da6bcee0a9a10d9c13d0e9a324334b39
אילון <eiloncap@gmail.com>
2021-06-26 22:00:26 +0300

    initiallized dialog

--- app/src/main/java/il/co/rootsapp/MainActivity.kt
+++ app/src/main/java/il/co/rootsapp/MainActivity.kt
@@ -14,17 +14,20 @@ class MainActivity : AppCompatActivity() {
 
     var viewModel: RootViewModel? = null
     private lateinit var adapter: RootsAdapter
+    private lateinit var rootsRecycleView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         viewModel = if (viewModel == null) viewModel else RootsApp.instance.viewModel
 
         val addButton = findViewById<FloatingActionButton>(R.id.addButton)
-        val rootsRecycleView = findViewById<RecyclerView>(R.id.rootsRecycleView)
+        rootsRecycleView = findViewById(R.id.rootsRecycleView)
 
         val alert = initDialog()
 
         rootsRecycleView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        initAdapter()
 
         addButton.setOnClickListener {
             alert.show()
@@ -33,15 +36,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun initAdapter() {
         adapter = RootsAdapter()
+        rootsRecycleView.adapter = adapter
     }
 
     private fun initDialog(): Dialog {
+        val v = layoutInflater.inflate(R.layout.input_alert, null)
         return AlertDialog.Builder(this)
             .setMessage("Enter a number to calculate:")
             .setCancelable(true)
-            .setView(layoutInflater.inflate(R.layout.input_alert, null))
+            .setView(v)
             .setPositiveButton("OK") { dialog, id ->
-                val numInput = findViewById<TextView>(R.id.numInput)
+                val numInput = v.findViewById<TextView>(R.id.numInput)
                 Log.d("eilon", numInput.text.toString())
                 if (numInput.text.isNotEmpty()) {
                     viewModel?.addNewItem(RootItem(num = numInput.text.toString().toInt()))
